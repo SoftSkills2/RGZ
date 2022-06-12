b4da90812b57775a36cf67f10e292bbfc74edad0
olgasherbiena <60242066+olgasherbiena@users.noreply.github.com>
2022-06-12 14:32:44 +0200

    12.06
    Зробила декомпозицію, видалила "тимчасову змінну" sc  і присвоювала "елементи на пряму", додала коментарі

--- project.cpp
+++ project.cpp
@@ -20,46 +20,77 @@ void ArrCompare(vector<string>& arr) {
             }
 }
 
-int main()
-{
-    vector <string> vec = {"string","string1","string2"};
-    vector <int> sizere = {0,0,0};
-    vector <int> scsave = {0,0,0};
-int n,sc;
-    for(int i=0;i<vec.size();i++)
-    {
-    cout<<"Enetr n - "<<endl;
+//input data
+void input_strings(vector<string> &vec){
+    std::cout << "Please enter count of strings: ";
+    int count;
+    std::cin >> count;
+    for(int i = 0; i < count; ++i){
+        string str;
+        std::cin >> str;
+        vec.push_back(str);
+    }
+}
+
+// this funtion receives strings, finds out to which character it is necessary to reduce the string and reduces
+void reduction_to_character(vector <int> &sizere, vector <int> &scsave, vector<string> &vec, const int &i){
+    cout<<"Enter n - ";
+    int n;
     cin>>n;
     sizere[i]=n;
-    sc=vec[i].size();
-    scsave[i]=sc;
+    scsave[i]=vec[i].size();
     vec[i].resize(n);
     cout<<vec[i];
     cout<<endl;
+}
+
+//this funtion add 3 points in the end of string
+void add3_points(vector <string> &vec, int &in, const int &i){
+    vec[i]+="...";
+    in=1;
+}
+
+//this funtion add 2 points in the end of string
+void add2_points(vector <string> &vec, int &in, const int &i){
+    vec[i]+="..";
+    in=1;
+}
+
+//this funtion add 1 points in the end of string
+void add1_points(vector <string> &vec, int &in, const int &i){
+    vec[i]+=".";
+    in=1;
+}
+
+//this funtion reduction strings to the entered character
+void reduction(vector <string> &vec){
+    vector <int> sizere = {0,0,0}; // array save size before reduction
+    vector <int> scsave = {0,0,0}; // array save size After reduction
+    
+    for(int i=0;i<vec.size();i++)
+    {
+      reduction_to_character(sizere, scsave, vec, i);  
     }
     ArrCompare(vec);
     for(int i=0;i<vec.size();i++)
     {
         int in=0;
         int j=scsave[i]-sizere[i];
-    if(j>=3){
-        vec[i]+="...";
-        in=1;
+        
+        if(j>=3)
+           add3_points(vec, in, i); 
+        if(j>=2&&in==0)
+            add2_points(vec, in, i);
+        if(j>=1&&in==0)
+            add1_points(vec, in, i);
+            
+        cout<<vec[i] << endl;
     }
-    if(j>=2&&in==0){
-        vec[i]+="..";
-        in=1;
-    }
-    if(j>=1&&in==0){
-        vec[i]+='.';
-        in=1;
-    }
-    cout<<vec[i];
-     cout<<endl;
-    }
+}
+int main()
+{
+    vector <string> vec; // save strings
+    input_strings(vec);
+    reduction(vec);
     return 0;
 }
-
-
-
-
